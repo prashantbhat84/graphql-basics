f88de3bcc8a61a2541c86f10699652ce93a1b81d
Prashant Bhat <prashantbhat84@gmail.com>
2020-03-21 20:40:51 +0530

    operation arguments done

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import { GraphQLServer } from "graphql-yoga";
 //Type Definations(application schema)
 const typeDefs = `
 type Query{
-    hello: String!,
-    name:String!,
-    location:String!,
-    bio:String!
+    greeting(name:String,position:String):String!
+    me:User!
+    post:Post!
+    add(val1:Float!,val2:Float):Float!
+}
+
+type User{
+    id:ID!
+    name:String!
+    email:String!
+    age:Int
+
+}
+type Post{
+    id:ID!
+    title:String!
+    body:String!
+    published:Boolean!
 }
 `;
 
@@ -14,17 +28,36 @@ type Query{
 
 const resolvers = {
   Query: {
-    hello() {
-      return "this is my first graphql query";
+    // four args parent(relational data)
+    // args operation args
+    // ctx context data
+    //info  operation info
+    greeting(parent, args, ctx, info) {
+      if (args.name) {
+        return `hello ! ${args.name} and you are hired as ${args.position}`;
+      }
+      return "hello";
     },
-    name() {
-      return "Prashant Bhat";
+    add(parent, args, ctx, info) {
+      if (!args.val2) {
+        return args.val1 + 50;
+      }
+      return args.val1 + args.val2;
     },
-    location() {
-      return "Bangalore";
+    me() {
+      return {
+        id: "abc123",
+        name: "Prashant Bhat",
+        email: "prashant@gmail.com"
+      };
     },
-    bio() {
-      return "I am a  software programmer";
+    post() {
+      return {
+        id: "alpa123",
+        title: "My graphql api",
+        body: "My own api",
+        published: false
+      };
     }
   }
 };
